fc87e4788a73d2c68291b897f7b658d10b56f6b9
chad-oneal <coneal@regscale.com>
2023-12-15 18:12:54 -0500

    Add Lines to PDF

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L',
              ln=1)
-    pdf.line(10, 21, 200, 21)
+    # sets lines on page
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer
-    pdf.ln(265)
+    pdf.ln(266)
     pdf.set_font(family='Times', style='I', size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
@@ -27,11 +29,13 @@ for index, row in df.iterrows():
     for i in range(row["Pages"] - 1):
         pdf.add_page()
         # Set the footer
-        pdf.ln(277)
+        pdf.ln(278)
         pdf.set_font(family='Times', style='I', size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
-
+        # sets lines on page
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 
 pdf.output('output.pdf')
